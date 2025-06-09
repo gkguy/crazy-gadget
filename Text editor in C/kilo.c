ca14128ae1a370cf6a2fb06348c1edd06991e18e
gkguy <geekamangira@gmail.com>
2025-06-09 01:14:18 +0100

    Added notes to demonstrate understanding of methods functionality as well as made overall progress. Raw mode is now disabled at exit to make it easy to cleanly exit from testing the code.

--- Text editor in C/kilo.c
+++ Text editor in C/kilo.c
@@ -1,12 +1,29 @@
+#include<stdlib.h>
 #include<termios.h>
-#include<unistd.h> 
+#include<unistd.h>
+ 
+struct termios orig_termios;
+
+void disableRawMode() {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+// Disbles raw mode (inputs are echoed again allowing for a clean process)
 
 void enableRawMode() {
-  struct termios raw;
-  tcgetattr(STDIN_FILENO, &raw);
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);
+
+  struct termios raw = orig_termios;  
   raw.c_lflag &= ~(ECHO);
+  
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
+// Reads the current terminal settings (i.e, canonical mode, etc) and stores it in the raw termios
+// structure. This allows for the bitwise fliping of the bits in ECHO to 
+// disable the echoing of inputs in the command line. Reads the current 
+// terminal settings with the current settings and discards the unread
+// input. This is so only the relevant keystrokes are processed.
+
 
 int main() 
 { 
@@ -16,3 +33,6 @@ int main()
     while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'); 
     return 0; 
 }
+// Reads 1 byte from standard input and stores it into c
+// as long as there are no more bytes to read.
+// Exits the input includes q
